a93b8e3eaefa3d7dd87f2e39f92d3888ec47042a
miatulcan <isabellamiatulcan@outlook.com>
2025-12-07 17:28:04 -0500

    Merge auth desde isa_nueva_rama (allow-unrelated-histories)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_page.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const RecipeBuddyApp());
 }
@@ -25,7 +28,6 @@ class RecipeBuddyApp extends StatelessWidget {
         "/home": (context) => const HomePage(),
       },
 
-      // 👇 PANTALLA INICIAL
       home: const LoginScreen(),
     );
   }
